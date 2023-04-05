5e03bf0529952b2f87fced665dc10547cc01c1ba
Zachary Siegel <zachary@etelie.com>
2023-04-04 23:53:57 -0500

    docker folder

--- src/env.mjs
+++ src/env.mjs
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from "zod"
 
 /**
  * Specify your server-side environment variables schema here. This way you can ensure the app isn't
@@ -8,15 +8,11 @@ const server = z.object({
   // Node
   NODE_ENV: z.enum(["development", "test", "production"]),
 
-  // Docker Compose
-  REPOSITORY_HOST: z.string().url(),
-  ORG_ID: z.string().min(1),
-  MODULE_ID: z.string().min(1),
-  PROJECT_VERSION: z.string().regex(/^\d+\.\d+\.\d+$/),
-  BUILD_NUMBER: z.number().or(z.string().regex(/^dev$/)),
+  // TeamCity
+  BUILD_TAG: z.string().optional(),
 
   // Prisma
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string(),
 
   // Next Auth
   NEXTAUTH_SECRET:
@@ -34,7 +30,7 @@ const server = z.object({
   // Next Auth Discord Provider
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
-});
+})
 
 /**
  * Specify your client-side environment variables schema here. This way you can ensure the app isn't
@@ -42,7 +38,7 @@ const server = z.object({
  */
 const client = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
-});
+})
 
 /**
  * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
@@ -53,11 +49,7 @@ const client = z.object({
 const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
 
-  REPOSITORY_HOST: process.env.REPOSITORY_HOST,
-  ORG_ID: process.env.ORG_ID,
-  MODULE_ID: process.env.MODULE_ID,
-  PROJECT_VERSION: process.env.PROJECT_VERSION,
-  BUILD_NUMBER: process.env.BUILD_NUMBER,
+  BUILD_TAG: process.env.BUILD_TAG,
 
   DATABASE_URL: process.env.DATABASE_URL,
 
@@ -68,39 +60,41 @@ const processEnv = {
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-};
+}
+
+console.debug(processEnv.DATABASE_URL)
 
 // Don't touch the part below
 // --------------------------
 
-const merged = server.merge(client);
+const merged = server.merge(client)
 
 /** @typedef {z.input<typeof merged>} MergedInput */
 /** @typedef {z.infer<typeof merged>} MergedOutput */
 /** @typedef {z.SafeParseReturnType<MergedInput, MergedOutput>} MergedSafeParseReturn */
 
-let env = /** @type {MergedOutput} */ (process.env);
+let env = /** @type {MergedOutput} */ (process.env)
 
 if (!process.env.SKIP_ENV_VALIDATION) {
-  const isServer = typeof window === "undefined";
+  const isServer = typeof window === "undefined"
 
   const parsed = /** @type {MergedSafeParseReturn} */ (
     isServer
       ? merged.safeParse(processEnv) // on server we can validate all env vars
       : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-  );
+  )
 
   if (!parsed.success) {
     console.error(
       "❌ Invalid environment variables:",
       parsed.error.flatten().fieldErrors
-    );
-    throw new Error("Invalid environment variables");
+    )
+    throw new Error("Invalid environment variables")
   }
 
   env = new Proxy(parsed.data, {
     get(target, prop) {
-      if (typeof prop !== "string") return undefined;
+      if (typeof prop !== "string") return undefined
       // Throw a descriptive error if a server-side env var is accessed on the client
       // Otherwise it would just be returning `undefined` and be annoying to debug
       if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
@@ -109,9 +103,9 @@ if (!process.env.SKIP_ENV_VALIDATION) {
             ? "❌ Attempted to access a server-side environment variable on the client"
             : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
-      return target[/** @type {keyof typeof target} */ (prop)];
+      return target[/** @type {keyof typeof target} */ (prop)]
     },
-  });
+  })
 }
 
-export { env };
+export { env }
