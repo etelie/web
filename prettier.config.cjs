89f90d9ac9348085229ad9e517ca099eab13a651
Zachary Siegel <zachary@etelie.com>
2023-04-05 23:11:02 -0500

    prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  trailingComma: "all",
+  printWidth: 100,
 };
 
 module.exports = config;
