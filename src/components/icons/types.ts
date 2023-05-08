cd2bb0e138b5d5934f97ada058d05ad73afe84f6
Zachary Siegel <zachary@etelie.com>
2023-05-07 23:55:10 -0500

    remove component groups types.ts exports

--- src/components/icons/types.ts
+++
@@ -1,3 +0,0 @@
-export type { EtelieIconProps } from './EtelieIcon';
-export type { ReloadIconProps } from './ReloadIcon';
-export type { BookmarkIconProps } from './BookmarkIcon';
--- src/components/layout/types.ts
+++
@@ -1,2 +0,0 @@
-export type { ModalProps } from './Modal';
-export type { TitleCardProps } from './TitileCard';
