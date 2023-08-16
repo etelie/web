684743329ecab971a4224593b1805af81a72cacb
Zachary Siegel <zachary@etelie.com>
2023-08-16 00:18:25 -0500

    clean server-side unused env

--- src/common/newsletter/endpoints.ts
+++ src/common/newsletter/endpoints.ts
@@ -1,9 +1,11 @@
 import type { EndpointBuilder } from "@/api";
 
+const basePath = '/newsletter'
+
 export const newsletterEndpoints = (builder: EndpointBuilder) => ({
   postNewsletterSubscription: builder.mutation<void, string>({
     query: emailAddress => ({
-      url: `/newsletter/subscription/${emailAddress}`,
+      url: `${basePath}/subscription/${emailAddress}`,
       method: 'POST',
       headers: {
         'content-type': undefined,
