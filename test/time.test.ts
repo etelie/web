99802800a485922f8ca2010ca1366e963e02d2c2
Zachary Siegel <zachary@etelie.com>
2023-08-16 23:55:15 -0500

    DateTimeFormat use dynamic locale (#27)

--- test/time.test.ts
+++ test/time.test.ts
@@ -4,6 +4,6 @@ import { renderTimestamp } from '@/common/time';
 
 describe('common/time', () => {
   test.each([new Date('1995-12-17T03:24:00')])('renderTimestamp', (date: Date) => {
-    expect(renderTimestamp(date)).toBe('December 17, 1995 – 3:24 AM CST');
+    expect(renderTimestamp(date, 'en-US')).toBe('December 17, 1995 – 3:24 AM CST');
   });
 });
