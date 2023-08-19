eedf5eeca57a8b1da4bb0142d7de692d2cb33425
Zachary Siegel <zachary@etelie.com>
2023-08-19 15:40:18 -0500

    init: form

--- src/components/layout/AuthPage/AuthPage.tsx
+++ src/components/layout/AuthPage/AuthPage.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx';
-import { useState } from 'react';
 
 import { TextInput, types as text_input_types } from '~/components/input/TextInput';
 import { Modal, sizes as modal_sizes } from '~/components/layout/Modal';
 import { Label } from '~/components/typography/Label';
+
+// todo: AuthItem.tsx
 
 type AuthItemProps = {
   label: string;
@@ -23,16 +24,38 @@ const AuthItem = ({ label, className, sensitive = false }: AuthItemProps) => {
   );
 };
 
+// todo: investigate Formik
+
+// todo: form folder
+
 export type AuthPageProps = {};
 
 export const AuthPage = ({}: AuthPageProps) => {
+  const labels = {
+    username: 'User name',
+    password: 'Password',
+  };
+
   return (
     <Modal title={'Authentication'} size={modal_sizes.sm}>
-      {/* todo: wrap with form */}
-      <div className={clsx('t-flex t-flex-col t-justify-between t-items-start', 't-mt-2')}>
-        <AuthItem label='User name' className={clsx('t-mb-2')} />
-        <AuthItem label='Password' sensitive />
-      </div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          const data = new FormData(e.currentTarget); // todo: create utility function for building and parsing FormData
+          const username = data.get(labels.username);
+          const password = data.get(labels.password);
+          console.log(`username: ${username}\npassword: ${password}`);
+        }}
+      >
+        <div className={clsx('t-flex t-flex-col t-justify-between t-items-start', 't-mt-2')}>
+          <AuthItem label={labels.username} className={clsx('t-mb-2')} />
+          <AuthItem label={labels.password} sensitive />
+          <div className={clsx('t-w-full', 't-flex t-flex-row t-justify-end')}>
+            {/* todo: styled submit button component */}
+            <button type='submit'>Submit</button>
+          </div>
+        </div>
+      </form>
     </Modal>
   );
 };
